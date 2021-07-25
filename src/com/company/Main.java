edce22f3c4362059ce11722e446882caac416ead
gulshoda10 <gulichka_rd@yahoo.com>
2021-07-24 23:05:46 -0700

    New commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-	  for (int i=0; i<5; i++){
+	  for (int i=0; i<6; i++){
           System.out.println("batch 9");
           Thread.sleep(1000);
       }
